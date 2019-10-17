960b498bfe60bba04f3d88bc68d8a5cddc01c99b
Lautaro Mónaco <lauti.monaco@hotmail.com>
2019-10-17 02:41:50 -0300

    Add files via upload

---
+++ Biblioteca - Lab/Equipos.h
@@ -0,0 +1,124 @@
+#ifndef EQUIPOS_H_INCLUDED
+#define EQUIPOS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LIBRE 0
+#define OCUPADO 1
+
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int anio;
+} sFecha;
+
+typedef struct
+{
+    int codigo;
+    char nombre[51];
+    char localidad[33];
+    int estado;
+} eEquipos;
+
+typedef struct
+{
+    int codigo;
+    char apellido[31];
+    char nombre[31];
+    int codigoDeEquipo;
+    char sexo;
+    sFecha fechaNacimiento;
+    int estado;
+} eJugadores;
+
+typedef struct
+{
+    int codigo;
+    char apellido[31];
+    char nombre[31];
+    char sexo;
+    char eMail[31];
+    sFecha fechaNacimiento;
+    int estado;
+} eReferi;
+
+typedef struct
+{
+    int codigo;
+    int codigoDeEquipoVisitante;
+    int codigoDeEquipoLocal;
+    int codigoDeReferi;
+    sFecha fechaDePartido;
+    int estado;
+} ePartidos;
+
+
+int onlyLetters(char []);
+char* enterString(char*);
+int enterInt(char []);
+float enterFloat(char []);
+int validarSexo(char);
+int validarEmail(char []);
+int relacionJugadorEquipo(eJugadores,eEquipos [],int);
+int relacionPartidoEquipoLocal(ePartidos, eEquipos [], int);
+int relacionPartidoEquipoVisitante(ePartidos, eEquipos [], int);
+int relacionPartidoReferi(ePartidos, eReferi [], int);
+int mostrarJugadoresPorEquipo(eJugadores [], eEquipos [], int,int);
+int mostrarReferiConMaePartidos(eReferi [],ePartidos [],int, int);
+int mostrarPartidosPorFecha(ePartidos [], int);
+void promedioEdadesPorEquipo(eJugadores [],eEquipos [],int, int);
+
+
+void hardcodeoEquipos(eEquipos [], int);
+int inicializarEquipos(eEquipos [], int);
+int cargarEquipo(eEquipos [], int, int);
+int buscarLibreEquipos(eEquipos [], int);
+void mostrarUnEquipo(eEquipos);
+void mostrarListadoDeEquipos(eEquipos [], int);
+int ordenarEquiposPorNombre(eEquipos [],int);
+int flagEquipos(eEquipos [], int);
+int modificarEquipo(eEquipos [],int);
+int buscarEquipoPorCodigo(eEquipos [], int );
+int bajarEquipo(eEquipos [], int);
+void mostrarEquipoMasLargo(eEquipos [], int);
+
+
+void hardcodeoJugadores(eJugadores [], int);
+int inicializarJugadores(eJugadores [], int);
+int cargarJugador(eJugadores [],eEquipos[],int, int, int);
+int buscarLibreJugadores(eJugadores [], int);
+void mostrarUnJugador(eJugadores);
+void mostrarListadoDeJugadores(eJugadores [], int);
+int ordenarJugadoresPorApellido(eJugadores [],int);
+int flagJugadores(eJugadores [], int);
+int buscarJugadorPorCodigo(eJugadores [], int );
+int bajarJugador(eJugadores [], int);
+int mostrarJugadoresPorNombre(eJugadores [],int);
+
+
+
+
+void hardcodeoReferees(eReferi [], int);
+int inicializarReferees(eReferi [], int);
+int cargarReferi(eReferi [], int, int);
+int buscarLibreReferees(eReferi [], int);
+void mostrarUnReferee(eReferi);
+void mostrarListadoDeReferees(eReferi [], int);
+
+
+
+
+void hardcodeoPartidos(ePartidos [], int);
+int inicializarPartidos(ePartidos [], int);
+int cargarPartido(ePartidos [],eEquipos[],eReferi[],int,int,int, int);
+int buscarLibrePartidos(ePartidos [], int);
+void mostrarUnPartido(ePartidos);
+void mostrarListadoDePartidos(ePartidos [], int);
+int ordenarPartidosPorFecha(ePartidos [],int cant);
+int flagPartidos(ePartidos [],int);
+
+
+#endif // EQUIPOS_H_INCLUDED
---
+++ Biblioteca - Lab/main.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Equipos.h"
+
+#define E 10
+#define J 35
+#define R 6
+#define P 25
+
+
+int main()
+{
+    eJugadores listaJugadores[J];
+    eEquipos listaEquipos[E];
+    eReferi listaReferi[R];
+    ePartidos listaPartidos[P];
+
+    int opcion;
+    int idJugador= 1;
+    int idEquipos = 1;
+    int idReferi = 1;
+    int idPartidos = 1;
+    int r;
+    char salir = 'n';
+
+    if((inicializarEquipos(listaEquipos, E)==0 && inicializarJugadores(listaJugadores, J)==0) &&
+            (inicializarReferees(listaReferi, R)==0 && inicializarPartidos(listaPartidos, P)==0))
+    {
+        do
+        {
+            printf("--------------BIENVENIDO--------------\n\n");
+            printf("1  -  ALTA EQUIPO\n");
+            printf("2  -  LISTAR EQUIPOS\n");
+            printf("3  -  ALTA JUGADOR\n");
+            printf("4  -  LISTAR JUGADORES\n");
+            printf("5  -  ALTA REFERI\n");
+            printf("6  -  ALTA PARTIDO\n");
+            printf("7  -  LISTAR PARTIDOS\n");
+            printf("8  -  MODIFICAR EQUIPO\n");
+            printf("9  -  BAJA DE EQUIPO\n");
+            printf("10 -  BAJA JUGADOR\n");
+            printf("11 -  MOSTRAR JUGADORES POR EQUIPO\n");
+            printf("12 -  REFERI CON MAS PARTIDOS DIRIGIDOS\n");
+            printf("13 -  MOSTRAR JUGADORES POR NOMBRE\n");
+            printf("14 -  EQUIPOS CON NOMBRE MAS LARGO.\n");
+            printf("15 -  MOSTRAR PARTIDOS POR FECHA\n");
+            printf("16 -  MOSTRAR EQUIPO MAS JOVEN\n");
+            printf("17 -  SALIR\n");
+
+            printf("Debe elegir una opcion a realizar: ");
+            fflush(stdin);
+            scanf("%d", &opcion);
+
+            switch(opcion)
+            {
+            case 1:
+                r=cargarEquipo(listaEquipos, E,idEquipos);
+                if(r==0)
+                {
+                    printf("Equipo cargado.\n");
+                    idEquipos ++;
+                }
+                else
+                {
+                    printf("No hay mas espacio de equipos.\n");
+                }
+                //hardcodeoEquipos(listaEquipos, E);   //Comienza con los datos ingresados
+                break;
+            case 2:
+                if(flagEquipos(listaEquipos, E)==0)
+                {
+                    ordenarEquiposPorNombre(listaEquipos, E);
+                    mostrarListadoDeEquipos(listaEquipos, E);
+                }
+                else
+                {
+                    printf("No hay equipos cargados.\n");
+                }
+                break;
+            case 3:
+                r=cargarJugador(listaJugadores,listaEquipos,J,E,idJugador);
+                if(r==0)
+                {
+                    printf("Jugador cargado exitosamente.\n\n");
+                    idJugador++;
+                }
+                else
+                {
+                    printf("No hay mas espacio de jugadores\n\n");
+                }
+                //hardcodeoJugadores(listaJugadores, J);
+                break;
+            case 4:
+                if(flagJugadores(listaJugadores, J)==0)
+                {
+                    ordenarJugadoresPorApellido(listaJugadores, J);
+                    mostrarListadoDeJugadores(listaJugadores,J);
+                }
+                else
+                {
+                    printf("No hay jugadores cargados\n");
+                }
+
+                break;
+            case 5:
+                r= cargarReferi(listaReferi, R,idReferi);
+                 if(r==0)
+                 {
+                     printf("Referi cargado\n");
+                     idReferi ++;
+                 }
+                 else
+                 {
+                     printf("No hay mas espacio de referees\n");
+                 }
+                //hardcodeoReferees(listaReferi, R);
+                break;
+            case 6:
+                r = cargarPartido(listaPartidos,listaEquipos,listaReferi, P,E,R,idPartidos);
+                if(r==0)
+                {
+                    printf("Partido cargado\n");
+                    idPartidos ++;
+                }
+                else
+                {
+                    printf("No hay mas espacio de partidos.\n");
+                }
+                //hardcodeoPartidos(listaPartidos, P);
+                break;
+            case 7:
+                if(flagPartidos(listaPartidos, P) ==0)
+                {
+                    ordenarPartidosPorFecha(listaPartidos, P);
+                    mostrarListadoDePartidos(listaPartidos, P);
+                }
+                else
+                {
+                    printf("No hay partidos cargados.\n");
+                }
+                break;
+            case 8:
+                if(flagEquipos(listaEquipos, E)==0)
+                {
+                    r = modificarEquipo(listaEquipos, E);
+                    if(r == 0)
+                    {
+                        printf("Equipo modificado.\n");
+                    }
+                    else if(r==1)
+                    {
+                        printf("Accion cancelada.\n");
+                    }
+                    else
+                    {
+                        printf("No se encontro al equipo.\n");
+                    }
+                }
+                else
+                {
+                    printf("No hay equipos cargados.\n");
+                }
+                break;
+            case 9:
+                if(flagEquipos(listaEquipos, E)==0)
+                {
+                    r = bajarEquipo(listaEquipos, E);
+                    if(r == 0)
+                    {
+                        printf("Equipo eliminado.\n");
+                    }
+                    else if(r==1)
+                    {
+                        printf("Accion cancelada.\n");
+                    }
+                    else
+                    {
+                        printf("No se encontro al equipo.\n");
+                    }
+                }
+                else
+                {
+                    printf("No hay equipos cargados.\n");
+                }
+                break;
+            case 10:
+                if(flagJugadores(listaJugadores, J)==0)
+                {
+                    r = bajarJugador(listaJugadores, J);
+                    if(r == 0)
+                    {
+                        printf("Jugador eliminado.\n");
+                    }
+                    else if(r==1)
+                    {
+                        printf("Accion cancelada.\n");
+                    }
+                    else
+                    {
+                        printf("No se encontro al Jugador.\n");
+                    }
+                }
+                else
+                {
+                    printf("No hay jugadores cargados.\n");
+                }
+                break;
+            case 11:
+                if(flagJugadores(listaJugadores, J)==0 && flagEquipos(listaEquipos, E)==0)
+                {
+                    r = mostrarJugadoresPorEquipo(listaJugadores,listaEquipos, J,E);
+                    if(r == 0)
+                    {
+                        printf("Jugadores listados con exito.\n");
+                    }
+                    else
+                    {
+                        printf("No se encontro al Jugador.\n");
+                    }
+                }
+                else
+                {
+                    printf("Faltan jugadores o equipos a cargar.\n");
+                }
+                break;
+            case 12:
+                break;
+            case 13:
+                if(flagJugadores(listaJugadores, J)==0)
+                {
+                    r = mostrarJugadoresPorNombre(listaJugadores, J);
+                    if(r==0)
+                    {
+                        printf("Jugadores mostrados exitosamente.\n");
+                    }
+                    else
+                    {
+                        printf("No se ha encontrado el nombre.\n");
+                    }
+                }
+                else
+                {
+                    printf("No hay jugadores cargados.\n");
+                }
+                break;
+            case 14:
+
+                break;
+            case 15:
+                if(flagPartidos(listaPartidos,P)==0)
+                {
+                    r=mostrarPartidosPorFecha(listaPartidos,P);
+                    if(r==0)
+                    {
+                        printf("Partidos listados exitosamente.\n");
+                    }
+                    else
+                    {
+                        printf("No se encontro ningun partido con esa fecha.\n");
+                    }
+                }
+                else
+                {
+                    printf("No hay partidos cargados.\n");
+                }
+                break;
+            case 16:
+                promedioEdadesPorEquipo(listaJugadores,listaEquipos,J,E);
+                break;
+
+            default:
+                printf("\nNo has elegido una opcion valida.\n\n");
+                break;
+            case 17:
+                printf("Desea salir del programa?: SI:'s' --- NO:'n'  \n");
+                fflush(stdin);
+                scanf("%c",&salir);
+                break;
+
+            }
+            system("pause");
+            system("cls");
+        }
+        while(salir == 'n');
+    }
+    else
+    {
+        printf("\nNo se pudo iniciar el menu\n");
+    }
+    system("cls");
+    printf("\nSaliendo del programa...\n");
+    return 0;
+}
